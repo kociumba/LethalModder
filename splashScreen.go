c920baaceedf54c3ed99a4be1f8bec47ba5e93ef
kociumba <79542688+kociumba@users.noreply.github.com>
2024-08-10 02:31:24 +0200

    hope this tells me what's wrong

--- splashScreen.go
+++ splashScreen.go
@@ -2,6 +2,6 @@ package main
 
 const splashScreenHTML string = `
 <div class="splash-screen">
-  <img src="https://raw.githubusercontent.com/kociumba/LethalModder/main/winres/splashScreen.png" class="splash-image" height="250" width="500"/>
+  <img src="https://raw.githubusercontent.com/kociumba/LethalModder/main/frontend/src/assets/splashScreen.png" class="splash-image" height="250" width="500"/>
 </div>
 `
