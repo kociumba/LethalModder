595091252579fa616dad55520a8241cbaffd9ca1
kociumba <79542688+kociumba@users.noreply.github.com>
2024-08-10 17:28:48 +0200

    I forgor 💀

--- splashScreen.go
+++ splashScreen.go
@@ -4,7 +4,12 @@ package main
 const splashScreenHTML string = `
 <body style="margin: 0; padding: 0; overflow: hidden">
 	<div class="splash-screen" style="display: flex; justify-content: center; align-items: center; height: 100vh; overflow: hidden;">
-    	<img src="https://raw.githubusercontent.com/kociumba/LethalModder/main/frontend/src/assets/splashScreen.png" class="splash-image" height="250" width="500"/>
+    	<img 
+			src="https://raw.githubusercontent.com/kociumba/LethalModder/main/frontend/src/assets/images/splashScreen.png" 
+			class="splash-image" 
+			height="250" 
+			width="500"
+			/>
 	</div>
 </body>
 `
